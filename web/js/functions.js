6084eac04ebcde9f40c1e86ffc35d7eb57e92e65
Hoermen <>
2018-07-23 17:48:10 +0200

    completed form.

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,7 +1,28 @@
+function modal() {
+    var radios = document.getElementsByName("payments");
+    if (radios[0].checked) {  
+        var x = document.getElementById("income");
+        var y = document.getElementById("outcome");
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        var x = document.getElementById("outcome");
+        var y = document.getElementById("income");
+        x.style.display = "block"; 
+        y.style.display = "none";
+    }
+}
+
 function income() {
-    console.log("in");
+    var x = document.getElementById("income");
+    var y = document.getElementById("outcome");
+    x.style.display = "block";
+    y.style.display = "none";
 }
 
 function outcome() {
-    console.log("out");
-}    
+    var x = document.getElementById("outcome");
+    var y = document.getElementById("income");
+    x.style.display = "block"; 
+    y.style.display = "none";
+} 
