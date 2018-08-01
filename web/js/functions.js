827721e923b7c311f890ba5d561f569c353fd488
TieDall <>
2018-08-01 22:21:43 +0200

    Insert pie chart in list.

--- web/js/functions.js
+++ web/js/functions.js
@@ -26,3 +26,35 @@ function outcome() {
     x.style.display = "block"; 
     y.style.display = "none";
 }
+
+function drawPieIn() {
+    var ctx = document.getElementById("piechartIncome");
+    var data = {
+        datasets: [{
+            data: [
+                1400, 
+                194, 
+                20,
+            ],
+            backgroundColor: [
+                'rgb(54, 162, 235)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+            ],
+            label: "Einnahmen"
+        }],
+        labels: [
+            'Gehalt',
+            'Sozialleistung',
+            'Geschenk',
+        ]
+    };
+    var options = {
+	responsive: true
+    };
+    var myPieChart = new Chart(ctx,{
+        type: 'pie',
+        data: data,
+        options: options
+    });
+}
