7c25c5df5b24683f57856021eb2502b0b9066b19
tiedall <>
2018-09-03 12:16:53 +0200

    Added new pool for hibernate (c3p0). Added WS-Client for Export (jax-ws).

--- src/java/managedBeans/Export.java
+++ src/java/managedBeans/Export.java
@@ -2,22 +2,32 @@ package managedBeans;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import ws.IOException_Exception;
+import ws.PDFGenerator;
+import ws.PDFGeneratorService;
 
 @Named(value = "export")
 @RequestScoped
 public class Export {
     
     String link;
+    String failureMessage;
     
     public Export() {
-        link = "";
+        link = "http://localhost/Piechart.pdf";
+        failureMessage = "";
     }
     
     /**
      * Calls SOAP-RPC on Plugin-Server for generating PDF export.
      */
     public void generate() {
-        
+        PDFGenerator pdf = new PDFGeneratorService().getPDFGeneratorPort();
+        try {
+            pdf.createPDF();
+        } catch (IOException_Exception ex) {
+            failureMessage = ex.toString();
+        }
     }
     
     /*
